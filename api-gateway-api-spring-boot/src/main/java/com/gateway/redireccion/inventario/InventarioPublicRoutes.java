ae262497c5457ec8f227e974da9a434364bf2c69
Omar Esteban Hernández Baro <omarehb10@gmail.com>
2025-06-10 22:36:35 -0400

    Api Gateway

---
+++ api-gateway-api-spring-boot/src/main/java/com/gateway/redireccion/inventario/InventarioPublicRoutes.java
@@ -0,0 +1,10 @@
+package com.gateway.redireccion.inventario;
+
+public class InventarioPublicRoutes {
+
+     public static final String[] GESTION_PUBLIC_GET = {
+        "/api/proxy/usuarios",
+        "/api/proxy/usuarios/"
+    };
+
+}
